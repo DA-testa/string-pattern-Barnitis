da17aa34c97d98fe14cddef8135c2909b1dc68ac
Bernhards Arnitis 221RDB128 <125700295+Barnitis@users.noreply.github.com>
2023-04-14 22:39:13 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,30 +1,63 @@
+# Bernhards Arnitis 221RDB128
 # python3
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
+    textinput = input()
+    # input
     # after input type choice
+    if "I" in textinput:
+          return (input().rstrip(), input().rstrip())
+    # input file
+    if "F" in textinput:
+        with open("./tests/06", 'r' ) as fr:
+          pattern = fr.readline().rstrip()
+          text = fr.readline().rstrip()
+    return pattern, text
+    
     # read two lines 
     # first line is pattern 
     # second line is text in which to look for pattern 
     
     # return both lines in one return
-    
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
-
+    
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    result = []
+    if not text or not pattern:
+        return result
+    
+    #prime numbers
+    num = 101
+    # patern hash
+    ph = 0
+    for char in pattern:
+        ph += ord(char)
+    ph %= num
+    # window hash
+    wh = 0
+    for j in range(len(pattern)):
+        wh += ord(text[j])
+    wh %= num
 
+    for i in range(len(text) - len(pattern) + 1):
+        if wh == ph:
+            if text[i:i+len(pattern)] == pattern:
+                result.append(i)
     # and return an iterable variable
-    return [0]
-
+        if i < len(text) - len(pattern):
+            wh -= ord(text[i])
+            wh += ord(text[i + len(pattern)])
+            wh %= num
+    
+    # return [0]
+    return result
 
 # this part launches the functions
 if __name__ == '__main__':
